4615ac2549b05fbfa05b7926b1c699616ec91b24
Charles <charleshu32@gmail.com>
2019-07-23 17:57:46 -0400

    Initial Commit

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Hu Chao Xiang on 2019-07-23.
+//  Copyright © 2019 Hu Chao Xiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
